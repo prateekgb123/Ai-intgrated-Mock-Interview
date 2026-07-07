2398185abaa38ac69938cf19dcb77b43bfb7ae69
prateekgb123 <prateekgb2004@gmail.com>
2026-07-07 12:30:27 +0530

    small changes made

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ai-intgrated-mock-interview-server-k50e.onrender.com/api",
 });
 
 export default API;
